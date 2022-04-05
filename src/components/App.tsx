92f7e403fecca41afa925c854957a574ec49936c
Daniel Frost <flint.bets@yandex.ru>
2022-04-05 11:32:41 +0300

    Created UserList view

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import { Admin } from 'react-admin';
+import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
+import { UserList } from './UserList';
 
 const App: React.FC = () => {
   const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
-  return <Admin dataProvider={dataProvider} />;
+
+  return (
+    <Admin dataProvider={dataProvider}>
+      <Resource name="users" list={UserList} />
+    </Admin>
+  );
 };
 
 export default App;
